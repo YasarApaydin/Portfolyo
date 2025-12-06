9f4de1226a1da09cce656f2e0ed4699e03782d50
Yaşar Apaydın <apaydinyasar5@gmail.com>
2025-12-07 02:43:14 +0300

    cvcvb

--- Portfolyo.WebApi/obj/Debug/net8.0/Portfolyo.WebApi.AssemblyInfo.cs
+++ Portfolyo.WebApi/obj/Debug/net8.0/Portfolyo.WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolyo.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d3d70fcdbe540e0be049ecd0736331ff6725422")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2023bdcaefca655d094f2e16d2bc47660b9f8322")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolyo.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolyo.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
