1227a2bda535316cd1bee7e0db48f3791befcd48
Yaşar Apaydın <apaydinyasar5@gmail.com>
2025-12-07 01:07:47 +0300

    Düzenleme 123454453223

--- Portfolyo.WebApi/wwwroot/js/login.js
+++ Portfolyo.WebApi/wwwroot/js/login.js
@@ -49,7 +49,7 @@
             loginMessage.classList.add("success-message");
 
             setTimeout(() => {
-                window.location.href = "/admin.html";
+                window.location.href = "https://yasarapaydinportfolyo-a5e0bwb5e8hrede5.westeurope-01.azurewebsites.net/admin";
             }, 1000);
 
 
