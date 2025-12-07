9770af3e43fef5b674ce55e453ed22085c8cc5e6
Yaşar Apaydın <apaydinyasar5@gmail.com>
2025-12-07 18:43:48 +0300

    sON bİTTİASDAD

--- Portfolyo.WebApi/wwwroot/js/login.js
+++ Portfolyo.WebApi/wwwroot/js/login.js
@@ -49,7 +49,7 @@
             loginMessage.classList.add("success-message");
 
             setTimeout(() => {
-                window.location.href = "/admin123.html";
+                window.location.href = "/admin.html";
             }, 1000);
 
 
